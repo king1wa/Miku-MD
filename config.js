0efbecf10a1c9d72914783fb9de95ca373b9da13
king1wa <111672626+king1wa@users.noreply.github.com>
2023-03-07 02:38:28 -0800

    Update config.js

--- config.js
+++ config.js
@@ -7,9 +7,9 @@ global.autoReadAll = false
 global.antitags = true
 
 
-global.Owner = ['918101187835']
-global.OwnerNumber = ['918101187835']
-global.ownertag = ['918101187835']
+global.Owner = ['923186401128']
+global.OwnerNumber = ['923186401128']
+global.ownertag = ['923186401128']
 global.BotName = "Miku"
 global.packname = "Miku bot"
 global.author = "By: Fantox"
